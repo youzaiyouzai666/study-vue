423f0a91d8196f32f6e14f2d346a229d1c974a10
youzaiyouzai666 <1620030379@qq.com>
2018-01-12 16:45:10 +0800

    Initial commit

--- todoMVC/src/app.js
+++ todoMVC/src/app.js
@@ -4,6 +4,7 @@ import  App from './components/App.vue';
 Vue.config.productionTip = false;//关闭生产提示
 new Vue({
     el: '#app',
+
     components: { App },//注册指令
     template: '<App/>',
 
--- todoMVC/src/components/App.vue
+++ todoMVC/src/components/App.vue
@@ -4,49 +4,140 @@
         <!-- header -->
         <header class="header">
             <h1>todos</h1>
-            <input class="new-todo"
-                   autofocus
-                   autocomplete="off"
-                   placeholder="What needs to be done?"
+            <input class="new-todo" placeholder="What needs to be done?" autofocus v-model="newTodo"
                    @keyup.enter="addTodo">
         </header>
         <!-- main section -->
-        <section class="main" v-show="todos.length">
-            <input class="toggle-all" id="toggle-all"
-                   type="checkbox"
-                   :checked="allChecked"
-                   @change="toggleAll({ done: !allChecked })">
-            <label for="toggle-all"></label>
+        <section class="main" v-show="todoList.length">
+            <input id="toggle-all" class="toggle-all" type="checkbox">
+            <label for="toggle-all">Mark all as complete</label>
             <ul class="todo-list">
-                <todo v-for="(todo, index) in filteredTodos" :key="index" :todo="todo"></todo>
+                <li v-for="todo in todoList" :class="{completed: todo.completed, editing: todo.editing}">
+                    <div class="view">
+                        <input class="toggle" type="checkbox" v-model="todo.completed">
+                        <label @dblclick="editTodo(todo)">{{todo.label}}</label>
+                        <button class="destroy" @click="removeTodo(todo)"></button>
+                    </div>
+                    <input class="edit" v-model="todo.label" v-auto-focus="todo.editing" @keyup.enter="finishEdit(todo)"
+                           @blur="finishEdit(todo)" @keyup.esc="cancelEdit(todo)">
+                </li>
             </ul>
         </section>
         <!-- footer -->
-        <footer class="footer" v-show="todos.length">
-      <span class="todo-count">
-        <strong>{{ remaining }}</strong>
-        {{ remaining | pluralize('item') }} left
-      </span>
+        <!-- This footer should hidden by default and shown when there are todos -->
+        <footer class="footer" v-show="todoList.length">
+            <!-- This should be `0 items left` by default -->
+            <span class="todo-count"><strong>{{remainingText}}</strong> item left</span>
+            <!--Remove this if you don't implement routing-->
             <ul class="filters">
-                <li v-for="(val, key) in filters">
-                    <a :href="'#/' + key"
-                       :class="{ selected: visibility === key }"
-                       @click="visibility = key">{{ key | capitalize }}</a>
+                <li>
+                    <a :class="{selected: visibility == 'all'}" href="#/">All</a>
+                </li>
+                <li>
+                    <a :class="{selected: visibility == 'active'}" href="#/active">Active</a>
+                </li>
+                <li>
+                    <a :class="{selected: visibility == 'completed'}" href="#/completed">Completed</a>
                 </li>
             </ul>
-            <button class="clear-completed"
-                    v-show="todos.length > remaining"
-                    @click="clearCompleted">
-                Clear completed
-            </button>
+            <!-- Hidden if no completed items are left ↓ -->
+            <button v-show="isCompleted" @click="removeCompleted()" class="clear-completed">Clear completed</button>
         </footer>
+
     </section>
 </template>
 
 
 <script>
     import Todo from './Todo.vue';
+
+    const filters = {
+        all      : function (todos) {
+            return todos;
+        },
+        active   : function (todos) {
+            return todos.filter(function (todo) {
+                return !todo.completed;
+            })
+        },
+        completed: function (todos) {
+
+            return todos.filter(function (todo) {
+                return todo.completed;
+            })
+        }
+    };
     export default {
-        components:{ Todo }
+        components: {Todo},
+        data() {
+            return {
+                todoList  : [],
+                newTodo   : '',
+                visibility: 'all'
+            }
+        },
+        directives: {
+            'auto-focus': function (el, binding) {
+                if (binding.value) {
+                    el.focus();
+                }
+            }
+        },
+        computed  : {
+            filteredTodos: function () {
+                return filters[this.visibility](this.todoList);
+            },
+            //active 剩余数量
+            remaining    : function () {
+                return filters.active(this.todoList).length;
+            },
+            remainingText: function () {
+                if (this.remaining === 0 || this.remaining > 1) {
+                    return this.remaining + 'items';
+                } else {
+                    return this.remaining + 'item';
+                }
+            },
+            //是否显示 “Clear completed”按钮
+            isCompleted  : function () {
+                return filters.completed(this.todoList).length > 0;
+            }
+        },
+        methods   : {
+            addTodo        : function () {
+                if (this.newTodo) {
+                    var todo = this.newTodo.trim();
+                    this.todoList.push({label: todo, completed: false, editing: false});
+                    this.newTodo = '';
+                }
+            },
+            removeTodo     : function (todo) {
+                var index = this.todoList.indexOf(todo);
+                this.todoList.splice(index, 1);
+            },
+            editTodo       : function (todo) {
+                this.editingTodoOldValue = todo.label;
+                todo.editing             = true;
+            },
+            finishEdit     : function (todo) {
+                if (!todo.editing) {
+                    return;
+                }
+                todo.editing = false;
+                if (!todo.label) {
+                    this.removeTodo(todo);
+                }
+            },
+            cancelEdit     : function (todo) {
+                todo.editing = false;
+                todo.label   = this.editingTodoOldValue;
+            },
+            /**
+             * 删除所有 completed
+             */
+            removeCompleted: function () {
+                this.todoList = filters.active(this.todoList);
+            }
+        }
     }
 </script>
