f6a66ccebe175f9cb39b78381d7c73690b5ea08e
youzaiyouzai666 <1620030379@qq.com>
2018-03-14 22:47:22 +0800

    vue ssr base1.0

--- ssr-base/server.js
+++ ssr-base/server.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const vueServerRenderer = require('vue-server-renderer');
 
 const app = express();
+
+// Server-Side Bundle File
+const serverBundleFilePath = path.join(__dirname, './dist/bundle.server.js')
+const serverBundleFileCode = fs.readFileSync(serverBundleFilePath, 'utf8');
+const bundleRenderer = vueServerRenderer.createBundleRenderer(serverBundleFileCode);
 
 // Client-Side Bundle File
 const clientBundleFilePath = path.join(__dirname, './dist/bundle.client.js');
@@ -11,8 +17,28 @@ const clientBundleFileUrl = '/bundle.client.js';
 
 // Server-Side Rendering
 app.get('/', function (req, res) {
-    const clientBundleFileCode = fs.readFileSync(path.join(__dirname, './dist/index.html'), 'utf8');
-    res.send(clientBundleFileCode);
+  bundleRenderer.renderToString((err, html) => {
+    console.log('html===',html);
+    if (err){
+      res.status(500).send(`
+        <h1>Error: ${err.message}</h1>
+        <pre>${err.stack}</pre>
+      `);
+    } else {
+      res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title>Vue 2.0 SSR</title>
+          </head>
+          <body>
+            ${html}
+            <script src="${clientBundleFileUrl}"></script>
+          </body>
+        </html>`);
+    }
+  });
       
   });
 
--- ssr-base/src/js/entry-server.js
+++ ssr-base/src/js/entry-server.js
@@ -2,3 +2,9 @@ import Vue from 'vue';
 import App from '../components/App.vue';
 
 const app = new Vue(App);
+
+export default function(context){
+    return new Promise((resolve, reject)=>{
+        resolve(app);
+    });
+}
